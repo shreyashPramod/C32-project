d8a9eb1047426daacf13bc8122b96156254fe281
shreyashPramod <65612631+shreyashPramod@users.noreply.github.com>
2020-07-17 23:56:54 -0700

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -79,6 +79,32 @@ function draw() {
   fill("red");
   text("SCORE :"+score,750,40);
 
+block1.score();
+block2.score();
+block3.score();
+block4.score();
+block5.score();
+block6.score();
+block7.score();
+block8.score();
+block9.score();
+block10.score();
+block11.score();
+block13.score();
+block14.score();
+block15.score();
+block16.score();
+
+blocks1.score();
+blocks2.score();
+blocks3.score();
+blocks4.score();
+blocks5.score();
+blocks6.score();
+blocks7.score();
+blocks8.score();
+blocks9.score();
+
   ground.display();
   stand1.display();
   stand2.display();
